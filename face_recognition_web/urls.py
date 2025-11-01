7eaf436e24ed9dfa83dcd5072e9c215b658e6673
MohammedRiyazdeen <ariyazdeen128@gmail.com>
2025-11-01 10:00:00 +0530

    Initial commit: Set up Django project structure

---
+++ face_recognition_web/urls.py
@@ -0,0 +1,10 @@
+"""
+URL configuration for face_recognition_web project.
+"""
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('recognition.urls')),
+]
